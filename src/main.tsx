811cd4ae7f10f19892081cb2e269bd3541d9f138
Sefa <76405419+sefabrt@users.noreply.github.com>
2024-07-10 00:21:15 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <TonConnectUIProvider manifestUrl="https://<YOUR_APP_URL>/tonconnect-manifest.json">
+    <TonConnectUIProvider manifestUrl="https://test-web3-airdrop-game.vercel.app/tonconnect-manifest.json">
       <App />
     </TonConnectUIProvider>
   </React.StrictMode>
